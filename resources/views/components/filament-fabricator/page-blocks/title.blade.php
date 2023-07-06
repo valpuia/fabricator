fc75348f4b47c24ee75597d219fe9ee58eb539f4
Lalhmangaihenga <valpuia604@gmail.com>
2023-07-06 17:35:24 +0700

    display simple data

--- resources/views/components/filament-fabricator/page-blocks/title.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/title.blade.php
@@ -1,7 +1,6 @@
 @aware(['page'])
 <div class="px-4 py-4 md:py-8">
     <div class="max-w-7xl mx-auto">
-        @dump($page)
-        {{-- {{ $page->blocks->title }} --}}
+        {{ $page->blocks[0]['data']['title'] }}
     </div>
 </div>
